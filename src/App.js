198e5634fac24c90285604ba098a9a91aa200594
Sara Munir <saramunir011@gmail.com>
2020-11-05 00:12:19 -0500

    m

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Notes from './components/notes'
+function App() {
+  return (
+    <div className="App">
+      Sara
+      <Notes/>
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/notes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function componentName() {
+  return (
+    <>
+    <div>
+        <div className="row container mx-auto">
+
+          <div class="card note">
+            <div class="card-header">
+              Featured
+            </div>
+            <div class="card-body">
+              <h5 class="card-title"><i class="fas fa-2x fa-plus"></i></h5>
+            </div>
+          </div>
+          <div class="card note">
+            <div class="card-header">
+              Featured
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+    </div>
+    </>
+  );
+}
